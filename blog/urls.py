e37361cf39f69e52ef40c167caf05be2b4f50847
wilfranduarte1 <wilfranduarte1@gmail.com>
2024-11-30 11:21:53 -0400

    post list

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import PostListView 
+from .views import PostListView , postDetailsView
+import pickle as pk
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'), 
+    path('post/<int:pk>/', postDetailsView.as_view(), name='post-detail'),
+
 ]
